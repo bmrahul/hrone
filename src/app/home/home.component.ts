6cb538819d70dd5be09b240a64b289405d90171f
Rahul Bansode <contact.rahulbm@gmail.com>
2022-04-21 13:08:26 +0530

    3 way toggle button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  model: any = {};
+  value = "na";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public options = [
+    { value: "on", id: "On" },
+    { value: "na", id: "NA" },
+    { value: "off", id: "Off" },
+  ]
+
+  onSubmit() {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model))
+  }
+
+  onSelectionChange(entry: any) {
+    debugger
+    this.value = entry;
+  }
 }
